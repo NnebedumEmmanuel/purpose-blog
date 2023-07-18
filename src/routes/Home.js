1894fad3c0276dde4300c7ad4acf160e832e918a
unknown <emmanuelnnebedum704@gmail.com>
2023-07-18 13:25:54 +0100

    update
    update

---
+++ src/routes/Home.js
@@ -0,0 +1,21 @@
+import HomeImg from "../assets/12.jpg";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
+
+
+function Home() {
+  return (
+    <>
+   
+    <Navbar /> 
+    <Hero 
+    cName="hero" heroImg={HomeImg} title="Your journey Your story" text="choose your favourite tours" buttonText="Travel plan" url="crop&w=1200&q=60" btnClass="show"
+    /> 
+     
+    </>
+   
+  );
+}
+
+export default Home;
