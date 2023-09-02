96d32f22fcb02c5a6c4e22a4ca4e555be3623d41
unknown <emmanuelnnebedum704@gmail.com>
2023-09-02 19:29:09 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="NavbarItems">
-        <h1 className="navbar-logo">C-lF</h1>
+        <h1 className="navbar-logo">JIreh Life</h1>
           
         <div className="menu-icons" onClick={this.handleClick}>
           <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
